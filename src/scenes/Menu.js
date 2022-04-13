964764afa37a06c2e8483dfd1155e2858ef3ac69
Grace Ho <grho@ucsc.edu>
2022-04-12 22:09:23 -0700

    animated menu screen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,29 +3,74 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
+    // var config = {
+    //   type: Phaser.AUTO,
+    //   width: 923,
+    //   height: 773,
+    //   parent: "phaser-example",
+    //   scene: {
+    //     preload: preload,
+    //     create: create
+    //   }
+    // };
+
+    //var game = new Phaser.Game(config);
+
+    preloadTitle() {
+      this.load.spritesheet("title", "assets/title_sheet.png", {
+        frameWidth: 923,
+        frameHeight: 773
+      });
+    }
+
+    createTitle() {
+      var config = {
+        key: "titleAnimation",
+        frames: this.anims.generateFrameNumbers("title", {
+          start: 0,
+          end: 7,
+          first: 0
+        }),
+        frameRate: 7,
+        repeat: -1
+      };
+
+      this.anims.create(config);
+  //   var bg = this.add.sprite(0,0,"title");
+  //   bg.setOrigin(0,0);
+      var bg = this.add.sprite(0, 0, "title").play("titleAnimation");
+      bg.setOrigin(0,0);
+    }
+
     preload() {
         // load audio
         this.load.audio('sfx_select', 'assets/select.wav');
         this.load.audio('sfx_explosion', 'assets/spaceship_exploding.wav');
         this.load.audio('sfx_rocket', 'assets/rocket_firing.wav');
-        // load title
-        this.load.image('title', 'assets/title.gif');
+        // load title png
+        this.load.image('title_png', './assets/title.png');
+        // load spritesheet 
+        // TOOL: https://ezgif.com/gif-to-sprite
+        this.load.spritesheet('title', './assets/title_sheet.png', {
+          frameWidth: 923, 
+          frameHeight: 773
+        });
     }
 
     create() {
-        // menu text config
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141', // title box color
-            color: '#843605', // title text color
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
+      // menu text config
+      let menuConfig = {
+          fontFamily: 'Courier',
+          fontSize: '28px',
+          backgroundColor: '#000000', // title box color
+          color: '#EE6EAB', // title text color
+          align: 'right',
+          padding: {
+              top: 5,
+              bottom: 5,
+          },
+          fixedWidth: 0
+      }
 
         // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
@@ -36,7 +81,7 @@ class Menu extends Phaser.Scene {
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    }
+      }
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
