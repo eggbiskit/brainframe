acca7a20cf7ef83b6cdf1eef9d2fc550b295a5e2
Grace Ho <grho@ucsc.edu>
2022-04-14 20:38:01 -0700

    point breakdown aka todo list

--- src/main.js
+++ src/main.js
@@ -8,8 +8,26 @@ let config = {
   
 let game = new Phaser.Game(config);
 // reserve keyboard vars
-let keyF, keyR, keyA, keyD, keyLEFT, keyRIGHT, keyENTER, keySHIFT, keyUP;
+let keyF, keyR, keyA, keyD, keyLEFT, keyRIGHT, keyENTER, keySHIFT, keyUP; // also always forgot to add new keys too
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
+/* POINT BREAKDOWN + JUSTIFICATION
+1. Implement a simultaneous two-player mode (30)
+  - player 1 uses keys A and D to move, F to fire
+  - player 2 uses left and right arrows keys to move, up arrow key to fire
+  - scores are on left and right sides for p1 and p2 respectively
+  - p1 chooses cell to play as first, then p2
+
+2. Redesign the game's artwork, UI, and sound to change 
+   its theme/aesthetic (to something other than sci-fi) (60)
+  - play as brain cells to shoot at negative thoughts
+  - ui is on theme - new menu, new borders, new scene for difficulty
+  - sound effects reflect mental anguish
+  - music is *chef's kiss*
+
+3. 
+
+TOTAL: 30 + 60 + .. = ...
+*/
